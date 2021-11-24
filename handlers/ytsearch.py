42806708d9f423ff834ccb787ef80ff51106a543
ssssfop <92902777+ssssfop@users.noreply.github.com>
2021-11-24 13:04:15 -0800

    Add files via upload

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -1,34 +1,53 @@
+import json
 import logging
-from pyrogram.types import Message
-from youtube_search import YoutubeSearch
-from pyrogram import Client as app
-from helpers.filters import command
+
 from config import BOT_USERNAME
+from helpers.filters import command
+from pyrogram import Client
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
+from youtube_search import YoutubeSearch
 
-
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-@app.on_message(command(["search", f"search@{BOT_USERNAME}"]))
+@Client.on_message(command(["search", f"search@{BOT_USERNAME}"]))
 async def ytsearch(_, message: Message):
+    
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "🗑 Close", callback_data="close",
+                )
+            ]
+        ]
+    )
+    
     try:
         if len(message.command) < 2:
-            await message.reply_text("/ بحث ** يحتاج الى حجة! **")
+            await message.reply_text("/search **needs an argument !**")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🎸 **جاري البحث..**")
+        m = await message.reply_text("🔎 **Searching...**")
         results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
         while i < 5:
-            text += f"🗼 **الاسم:** __{results[i]['title']}__\n"
-            text += f"🍥 **الوقت:** `{results[i]['duration']}`\n"
-            text += f"🦹🏻 **المشاهدات:** `{results[i]['views']}`\n"
-            text += f"🗽 **القناه:** {results[i]['channel']}\n"
-            text += f"🎟: https://www.youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"🏷 **Name:** __{results[i]['title']}__\n"
+            text += f"⏱ **Duration:** `{results[i]['duration']}`\n"
+            text += f"👀 **Views:** `{results[i]['views']}`\n"
+            text += f"📣 **Channel:** {results[i]['channel']}\n"
+            text += f"🔗: https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
-        await m.edit(text, disable_web_page_preview=True)
+        await m.edit(text, reply_markup=keyboard, disable_web_page_preview=True)
     except Exception as e:
         await m.edit(str(e))
